796dda2ac85eb557eac26819118d8ed6af6cd9e9
WebArtixan <umaralihashmi@gmail.com>
2026-04-27 18:55:15 +0500

    AOS Added! commit

--- js/custom.js
+++ js/custom.js
@@ -180,7 +180,7 @@
 
       // Auto play - medium speed
       autoplay: {
-        delay: 300000,
+        delay: 3000,
         disableOnInteraction: false,
         pauseOnMouseEnter: true,
       },
